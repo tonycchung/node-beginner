5d74dccabd8901fa91c31d69c12435b9758ea4c1
Tony Chung <anthonyuchung@gmail.com>
2014-09-10 22:10:53 -0700

    /upload displays postData we sent.

--- requestHandlers.js
+++ requestHandlers.js
@@ -24,7 +24,7 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
 
